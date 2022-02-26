ed1dab3935d08fbdea8cec5ef793acd6d206a0f6
roberto-medeiros <robertomedeirosjr@live.com>
2022-02-25 22:12:26 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const { lstat } = require("fs")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(process.env.PORT ||5500, ()=>{
+    console.log("servidor rodando")
+})
